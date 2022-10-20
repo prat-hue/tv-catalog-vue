53721b5d25ba479d7a971bda8e1a90bdb7b61672
Prathiba <prathiba.sumathikannappan@bam.com>
2022-10-20 16:22:44 +0200

    more tests

---
+++ src/components/__tests__/Menu.spec.ts
@@ -0,0 +1,20 @@
+import {describe, it, expect} from "vitest";
+
+import {flushPromises, mount} from "@vue/test-utils";
+import Menu from "@/components/Menu.vue";
+
+describe('Menu', () => {
+    it('Should load two menu items', () => {
+        const wrapper = mount(Menu);
+        expect(wrapper.text()).toContain("Home");
+        expect(wrapper.text()).toContain("Search");
+        expect(wrapper.findAll('#menu-item').length).toBe(2);
+    });
+    it('should call onPageChange when click a page item', async () => {
+        const wrapper = mount(Menu);
+        await wrapper.find('RouterLink').trigger('click');
+        expect(wrapper.html()).toContain('Home');
+        expect(wrapper.html()).toContain('Search')
+
+    })
+})
